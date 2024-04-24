e81a2e6ecd982bb1e9096d67803edd0745dc115d
Francisco Gómez <157551543+Kcx46@users.noreply.github.com>
2024-04-23 23:55:39 -0600

    Update POLYVSCODE.lisp

--- POLYVSCODE.lisp
+++ POLYVSCODE.lisp
@@ -1,3 +1,15 @@
+;This code makes several operations using a polynomial as an input:
+;zero(x)= 0
+;p(x)= + 4x^3 + 3x^2 + 2x + 1 ;;q(x)= + 3x^2 + 5 
+;p(x) + q(x) = + 4x^3 + 6x^2 + 2x + 6 
+;p(x) * q(x) = + 12x^5 + 9x^4 + 26x^3 + 18x^2 + 10x + 5 
+;p(q(x))= + 108x^6 + 567x^4 + 996x^2 + 586 
+;0-p(x)= -4x^3 -3x^2 -2x -1 
+;p(3)=142
+;p '(x)= + 12x^2 + 6x + 2 
+;p''(x)= + 24x + 6 ;
+
+
 (defvar *polynomial* '())
 (defvar *polynomial2* '())
 (defvar *polynomial-result* '())
